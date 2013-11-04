9cf08cc2f4815547743a56cd1c0407ec9f75dc0a
Barend Garvelink <bgarvelink@xebia.com>
2013-11-04 22:52:40 +0100

    Modifies Android build to get Parse apikey from env var.

--- Sebastiaanschool/src/parse/java/nl/sebastiaanschool/contact/app/SebApp.java
+++ Sebastiaanschool/src/parse/java/nl/sebastiaanschool/contact/app/SebApp.java
@@ -10,14 +10,11 @@ import com.parse.ParseObject;
  */
 public class SebApp extends Application {
 
-    private static final String APPLICATION_ID = "your app ID could be in here!";
-    private static final String CLIENT_KEY = "just enter it and you're set!";
-
     @Override
     public void onCreate() {
         super.onCreate();
         ParseObject.registerSubclass(AgendaItem.class);
         Parse.setLogLevel(BuildConfig.DEBUG ? Parse.LOG_LEVEL_DEBUG : Parse.LOG_LEVEL_NONE);
-        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+        Parse.initialize(this, ParseConfig.APPLICATION_ID, ParseConfig.CLIENT_KEY);
     }
 }
