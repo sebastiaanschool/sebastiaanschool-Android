75c42db6232a12862e713c3064a39965bd7b4ec5
Barend Garvelink <bgarvelink@xebia.com>
2016-05-06 22:42:01 +0100

    Adds a method to obtain newsletter download size.

--- Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/data/BackendInterface.java
+++ Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/data/BackendInterface.java
@@ -1,19 +1,28 @@
 package nl.sebastiaanschool.contact.app.data;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.squareup.moshi.Moshi;
 
 import java.io.File;
+import java.io.IOException;
 
 import nl.sebastiaanschool.contact.app.BuildConfig;
 import nl.sebastiaanschool.contact.app.data.server.BackendApi;
 import nl.sebastiaanschool.contact.app.data.server.TimelineJsonConverter;
 import okhttp3.Cache;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.moshi.MoshiConverterFactory;
+import rx.Single;
+import rx.SingleSubscriber;
 
 /**
  * Singleton provider/accessor for our backend connection.
@@ -24,6 +33,7 @@ public class BackendInterface {
     private static BackendInterface instance;
 
     public final BackendApi connector;
+    private final OkHttpClient okHttpClient;
 
     public static synchronized void init(Context context) {
         if (instance != null) {
@@ -41,7 +51,7 @@ public class BackendInterface {
 
     private BackendInterface(File cacheDir) {
         final Cache cache = new Cache(cacheDir, CACHE_SIZE_BYTES);
-        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+        okHttpClient = new OkHttpClient.Builder()
                 .cache(cache)
                 .build();
         final Moshi moshi = new Moshi.Builder()
@@ -55,5 +65,44 @@ public class BackendInterface {
                 .build();
         connector = retrofit
                 .create(BackendApi.class);
+    }
+
+    /**
+     * Returns a cold observable that yields the content-length of the given URL.
+     * @param target an URL.
+     * @return its content-length, or {@code -1} if unknown.
+     */
+    public Single<Long> getDownloadSize(final HttpUrl target) {
+        return Single.create(new Single.OnSubscribe<Long>() {
+            @Override
+            public void call(final SingleSubscriber<? super Long> subscriber) {
+                Request req = new Request.Builder()
+                        .head()
+                        .url(target)
+                        .addHeader("Accept", "application/pdf,image/*")
+                        .build();
+                okHttpClient.newCall(req).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.d("BINF", "Download size error: " + e + " of " + target);
+                        subscriber.onError(e);
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        final String contentLength = response.header("Content-Length", "-1");
+                        long result;
+                        try {
+                            result = Long.parseLong(contentLength);
+                        } catch (NumberFormatException e) {
+                            Log.d("BINF", "Download size: NFE on " + contentLength + " of " + target);
+                            result = -1;
+                        }
+                        Log.d("BINF", "Download size: " + result + " of " + target);
+                        subscriber.onSuccess(result);
+                    }
+                });
+            }
+        });
     }
 }
