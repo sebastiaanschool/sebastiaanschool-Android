a1d17c5e68bc358c8fafb4890641b5d8833fb618
Barend Garvelink <bgarvelink@xebia.com>
2016-07-30 00:55:34 +0200

    Add contentDescription to DownloadStatusView.

--- Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/gui/DownloadStatusView.java
+++ Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/gui/DownloadStatusView.java
@@ -2,7 +2,9 @@ package nl.sebastiaanschool.contact.app.gui;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.text.format.Formatter;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -17,11 +19,20 @@ import nl.sebastiaanschool.contact.app.R;
  */
 public class DownloadStatusView extends FrameLayout {
 
+    @DrawableRes
     private static final int[] STATUS_ICONS = {
             R.drawable.ic_download_pending_24dp,
             R.drawable.ic_download_in_progress_24dp,
             R.drawable.ic_download_complete_24dp,
             R.drawable.ic_download_failed_24dp
+    };
+
+    @StringRes
+    private static final int[] CONTENT_DESCRIPTIONS = {
+            R.string.download_pending,
+            R.string.download_in_progress,
+            R.string.download_complete,
+            R.string.download_failed
     };
 
     private ImageView icon;
@@ -78,5 +89,6 @@ public class DownloadStatusView extends FrameLayout {
     private void updateStatusImage(@DownloadStatus.DownloadStatusCode int statusCode) {
         this.icon.setImageDrawable(GrabBag.loadVectorDrawable(getContext(),
                 STATUS_ICONS[statusCode]));
+        this.icon.setContentDescription(getContext().getString(CONTENT_DESCRIPTIONS[statusCode]));
     }
 }
