sebastiaanschool/sebastiaanschool-Android

commit cf27461bfaa78798252103345b1c14528bb4828c

Move all initialization into MainActivity.
Remove the SebApp class, because the Firebase Crash Reporter relies on a child
process. Any initialization happening in the Application class causes a world
of hurt once child processes are in play.

Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/MainActivity.java
@@ -11,7 +11,9 @@ package nl.sebastiaanschool.contact.app;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.TabLayout;
+import android.support.v4.os.TraceCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -35,9 +37,8 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("NewApi")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        JodaTimeAndroid.init(this);
-        BackendInterface.init(this);
-        DownloadManagerInterface.init(this);
+        initializeStrictMode();
+        initializeApplicationServices();
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.action_bar));
 
@@ -46,5 +47,34 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tl = (TabLayout) findViewById(R.id.detail_tabs);
         tl.setupWithViewPager(vp);
+    }
+
+    private void initializeApplicationServices() {
+        TraceCompat.beginSection("Initialize services");
+        try {
+            JodaTimeAndroid.init(this);
+            BackendInterface.init(this);
+            DownloadManagerInterface.init(this);
+        } finally {
+            TraceCompat.endSection();
+        }
+    }
+
+    private void initializeStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .penaltyDeathOnNetwork()
+                    .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .setClassInstanceLimit(BackendInterface.class, 1)
+                    .setClassInstanceLimit(DownloadManagerInterface.class, 1)
+                    .build());
+        }
     }
 }

Sebastiaanschool/src/main/AndroidManifest.xml
@@ -25,7 +25,6 @@
     <uses-permission android:name="com.google.android.c2dm.permission.RECEIVE" />
 
     <application
-        android:name=".SebApp"
         android:allowBackup="false"
         android:icon="@mipmap/ic_launcher"
         android:label="@string/app_name"

Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/SebApp.java
@@ -1,37 +1,0 @@
-/*
- Copyright (c) 2014 Barend Garvelink
-
- SebastiaanSchool is licensed under a
- Creative Commons Attribution-NonCommercial 3.0 Unported License.
-
- You should have received a copy of the license along with this
- work.  If not, see <http://creativecommons.org/licenses/by-nc/3.0/>.
- */
-package nl.sebastiaanschool.contact.app;
-
-import android.app.Application;
-import android.os.StrictMode;
-
-import nl.sebastiaanschool.contact.app.data.BackendInterface;
-
-public class SebApp extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if (BuildConfig.DEBUG) {
-            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                    .detectAll()
-                    .penaltyLog()
-                    .penaltyFlashScreen()
-                    .penaltyDeathOnNetwork()
-                    .build());
-
-            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                    .detectAll()
-                    .penaltyLog()
-                    .setClassInstanceLimit(BackendInterface.class, 1)
-                    .build());
-        }
-    }
-}

commit 2454879bfbad68792bd952e74d27a3b1425860a1

Dispose of DownloadManagerInterface in onDestroy.
This fixes a bug introduced in cf27461bfaa78798252103345b1c14528bb4828c. Prior
to that commit, the DMI was bound to the Application object and there was no
reason to unregister the download receiver. Now that it's bound to the main
activity, there is.

Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/MainActivity.java
@@ -62,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        DownloadManagerInterface.getInstance().destroy(this);
+    }
+
     private void initializeStrictMode() {
         if (BuildConfig.DEBUG) {
             StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()

Sebastiaanschool/src/main/java/nl/sebastiaanschool/contact/app/data/downloadmanager/DownloadManagerInterface.java
@@ -63,6 +63,11 @@ public class DownloadManagerInterface {
         context.registerReceiver(downloadCompletionReceiver, filter);
     }
 
+    public void destroy(Context context) {
+        context.unregisterReceiver(downloadCompletionReceiver);
+        instance = null;
+    }
+
     /**
      * Returns a hot observable that emits status updates for downloaded files.
      * @return download observable.